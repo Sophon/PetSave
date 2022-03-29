65c7a5fe34f41ccab22db62638911c39232ff1f4
Le Duy Anh <LeNDuyA@gmail.com>
2022-03-29 17:30:06 +0200

    Create base and animals endpoints

--- app/src/main/java/com/example/petsaveedu/common/data/api/ApiConstants.kt
+++ app/src/main/java/com/example/petsaveedu/common/data/api/ApiConstants.kt
@@ -2,6 +2,10 @@ package com.example.petsaveedu.common.data.api
 
 object ApiConstants {
 
+    const val BASE_ENDPOINT = "https://api.petfinder.com/v2/"
+
+    const val ANIMALS_ENDPOINT = "animals"
+
     const val KEY = "GwWHyrovNzYR1OQAQCA8bGMSylmO38WxFK99DQfhUVwE1UVj8S"
     const val SECRET = "8gIYjmZy0zhalGwu8hwo1ZlqrGDKzHwuyvSWefLG"
 
