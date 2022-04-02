df937e037bdb15ce82a4ca726640455d2d79c791
Le Duy Anh <LeNDuyA@gmail.com>
2022-04-03 01:03:33 +0200

    Create Api module
    And connect authentication interceptor.

---
+++ app/src/main/java/com/example/petsaveedu/common/data/di/ApiModule.kt
@@ -0,0 +1,43 @@
+package com.example.petsaveedu.common.data.di
+
+import com.example.petsaveedu.common.data.api.ApiConstants
+import com.example.petsaveedu.common.data.api.PetFinderApi
+import com.example.petsaveedu.common.data.api.interceptors.AuthenticationInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(builder: Retrofit.Builder): PetFinderApi {
+        return builder
+            .build()
+            .create(PetFinderApi::class.java)
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .baseUrl(ApiConstants.BASE_ENDPOINT)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        authenticationInterceptor: AuthenticationInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authenticationInterceptor)
+            .build()
+    }
+}
