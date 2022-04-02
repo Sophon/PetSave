2f22b196e5bdf60469f253add7e1e31ea90ebbf1
Le Duy Anh <LeNDuyA@gmail.com>
2022-04-03 00:58:57 +0200

    Implement authentication interceptor

--- app/src/main/java/com/example/petsaveedu/common/data/preferences/Preferences.kt
+++ app/src/main/java/com/example/petsaveedu/common/data/preferences/Preferences.kt
@@ -1,7 +1,15 @@
 package com.example.petsaveedu.common.data.preferences
 
+import com.example.petsaveedu.common.data.api.model.ApiToken
+
 interface Preferences {
 
+    fun getToken(): String
+    fun getTokenExpirationTime(): Long
 
+    fun saveToken(token: String)
+    fun saveTokenExpirationTime(time: Long)
+    fun saveTokenType(type: String)
 
+    fun deleteToken()
 }
