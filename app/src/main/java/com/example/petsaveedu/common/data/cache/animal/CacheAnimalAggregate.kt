51e57162e7cbe4a89e02896f65fc7fc09b1090b6
Le Duy Anh <lenduya@gmail.com>
2022-04-04 16:48:47 +0200

    Create relation classes

---
+++ app/src/main/java/com/example/petsaveedu/common/data/cache/animal/CacheAnimalAggregate.kt
@@ -0,0 +1,22 @@
+package com.example.petsaveedu.common.data.cache.animal
+
+import androidx.room.Embedded
+import androidx.room.Junction
+import androidx.room.Relation
+
+data class CacheAnimalAggregate(
+    @Embedded val animal: CacheAnimalWithDetails,
+
+    @Relation(parentColumn = "animalId", entityColumn = "animalId")
+    val videos: List<CacheVideo>,
+
+    @Relation(parentColumn = "animalId", entityColumn = "animalId")
+    val photos: List<CachePhoto>,
+
+    @Relation(
+        parentColumn = "animalId",
+        entityColumn = "tag",
+        associateBy = Junction(CacheAnimalTagCrossRef::class)
+    )
+    val tags: List<CacheTag>
+)
